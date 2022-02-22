1d173bd25d14446ec8a1d99a6e66af727b65f4e6
izzul-hilmi <2001177A@student.tp.edu.sg>
2022-02-22 22:26:06 +0800

    Created UserTestng file and added codes for testing register

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,39 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class UserTest {
+  @Test
+  public void f() {
+	  
+
+	  System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+
+	  WebDriver driver = new ChromeDriver();
+
+	  driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+
+	  driver.findElement(By.linkText("Register")).click();
+	  
+	  driver.findElement(By.name("yourName")).sendKeys("JohnDoe3");
+	  driver.findElement(By.name("yourPassword")).sendKeys("123");
+	  driver.findElement(By.name("yourEmail")).sendKeys("johndoe3@gmail.com");
+	  
+	  driver.findElement(By.id("submit_")).click();
+	  
+	  driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+	  
+//	  driver.navigate().refresh();  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
