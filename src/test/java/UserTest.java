4dca488eb1030af1dbd6e934293199912f552784
izzul-hilmi <2001177A@student.tp.edu.sg>
2022-02-24 17:34:51 +0800

    changed usertest var

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -20,48 +20,38 @@ public class UserTest {
 
 	  driver.findElement(By.linkText("Register")).click();
 	  
-	  var name = driver.findElement(By.name("name"));
-	  var password = driver.findElement(By.name("password"));
-	  var email = driver.findElement(By.name("email"));
-	  var submit = driver.findElement(By.id("submit_"));
-	  
 //	  driver.findElement(By.name("name")).sendKeys("JohnDoe3");
 //	  driver.findElement(By.name("password")).sendKeys("123");
 //	  driver.findElement(By.name("email")).sendKeys("johndoe3@gmail.com");
 	  
-	  name.sendKeys("JohnDoe3");
-	  password.sendKeys("123");
-	  email.sendKeys("johndoe3@gmail.com");
+	  driver.findElement(By.name("name")).sendKeys("JohnDoe3");
+	  driver.findElement(By.name("password")).sendKeys("123");
+	  driver.findElement(By.name("email")).sendKeys("johndoe3@gmail.com");
 	  
-	  submit.click();
+	  driver.findElement(By.id("submit_")).click();
 	  
 	  driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
 	  
 	  //update
 	  driver.findElement(By.xpath("//a[@href='UserServlet/edit?name=JohnDoe3']")).click();
 	  
-	  var name2 = driver.findElement(By.name("name"));
-	  var password2 = driver.findElement(By.name("password"));
-	  var email2 = driver.findElement(By.name("email"));
-	  var submit2 = driver.findElement(By.id("submit_"));
 	  
-	  name2.clear();
-	  name2.sendKeys("JohnDoe3Edited");
+	  driver.findElement(By.name("name")).clear();
+	  driver.findElement(By.name("name")).sendKeys("JohnDoe3Edited");
 	  
-	  password2.clear();
-	  password2.sendKeys("1234");
+	  driver.findElement(By.name("password")).clear();
+	  driver.findElement(By.name("password")).sendKeys("1234");
 	  
-	  email2.clear();
-	  email2.sendKeys("johndoe3edited@gmail.com");
+	  driver.findElement(By.name("email")).clear();
+	  driver.findElement(By.name("email")).sendKeys("johndoe3edited@gmail.com");
 
-	  submit2.click();
+	  driver.findElement(By.id("submit_")).click();
 	  
 	  driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
 	  
 	  //delete
 	  driver.findElement(By.xpath("//a[@href='UserServlet/delete?name=JohnDoe3']")).click();
-	  
-	  driver.quit();
+
 	  
 //	  driver.findElement(By.xpath("//linkText")).click();
 	  
