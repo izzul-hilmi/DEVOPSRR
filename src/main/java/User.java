267e7d87cf307cc0111ed48375c4614b5813549e
izzul-hilmi <2001177A@student.tp.edu.sg>
2022-02-26 02:22:36 +0800

    Junit test
    Edited the UserTest file to separate the functions to make it more
    easier to read.
    Transferred codes from functions of UserServlet into User.java so that
    Junit Testing can be applied into the newly created UserJUTest file

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,9 @@
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class User {
 
@@ -27,5 +33,114 @@ public class User {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	// Define variables
+	private static String jdbcURL = "jdbc:mysql://localhost:3306/restaurant";
+	private static String jdbcUsername = "root";
+	private static String jdbcPassword = "password";
+	
+	// Define connection
+	protected static Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	public static int registerUser(String formName, String formPassword, String formEmail) throws SQLException {
+		int i = 0;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "password");
+
+			// Step 4: implement the sql query using prepared statement
+			// (https://docs.oracle.com/javase/tutorial/jdbc/basics/prepared.html)
+			PreparedStatement ps = con.prepareStatement("insert into USERINFO values(?,?,?)");
+
+			// Step 5: parse in the data retrieved from the web form request into the
+			// prepared statement accordingly
+			ps.setString(1, formName);
+			ps.setString(2, formPassword);
+			ps.setString(3, formEmail);
+
+			// Step 6: perform the query on the database using the prepared statement
+			i = ps.executeUpdate();
+			// Step 7: check if the query had been successfully execute, return “You are
+			// successfully registered” via the response,
+			if (i > 0) {
+				
+				System.out.println("Successful !!!!!!!ftyujnvfgtyhnb   "+ i);
+				return i;
+			}
+		}
+		// Step 8: catch and print out any exception
+		catch (Exception exception) {
+			System.out.println(exception);
+		}
+		System.out.println(i);
+		return i;
+		
+	}
+
+//	public static int showEditForm(String name, User existingUser) throws SQLException {
+//		
+//		int i = 0;
+//		
+//		// Step 1: Establishing a Connection
+//		try (
+//				Connection connection = getConnection();
+//				// Step 2:Create a statement using connection object
+//				PreparedStatement preparedStatement = connection.prepareStatement("select name,password,email from userinfo where name =?;");
+//			) 
+//		{
+//			preparedStatement.setString(1, name);
+//				
+//			// Step 3: Execute the query or update query
+//			ResultSet rs = preparedStatement.executeQuery();
+//			
+//			// Step 4: Process the ResultSet object
+//			while (rs.next()) {
+//				name = rs.getString("name");
+//				String password = rs.getString("password");
+//				String email = rs.getString("email");
+//				existingUser = new User(name, password, email);
+//				i = name.length();
+//			}
+//		} catch (SQLException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		return i;
+//	}
+	
+	public static int updateUser(String oriName, String name, String password, String email) throws SQLException {
+		
+		//Step 2: Attempt connection with database and execute update user SQL query
+		try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement("update UserInfo set name = ?,password= ?, email =? where name = ?;");) {
+			statement.setString(1, name);
+			statement.setString(2, password);
+			statement.setString(3, email);
+			statement.setString(4, oriName);
+			int i = statement.executeUpdate();
+			return i;
+		} 
+				
+	}
+	
+	public static int deleteUser(String name) throws SQLException {
+		
+		try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement("delete from UserInfo where name = ?;");) 
+		{
+			statement.setString(1, name);
+			int i = statement.executeUpdate();
+			return i;
+		}
 	}
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -6,55 +6,102 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class UserTest {
-  @Test
-  public void f() {
-	  
-	// define the chrome driver
-      System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+	
+	
+	@Test(priority=1)
+	public void register() {
 
-       // define the drive instance
-      WebDriver driver = new ChromeDriver();
-      driver.manage().window().maximize();
-      // nagivate the browser to this url
-      driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
-      // Navigate to this button to create the booking
-//      driver.findElement(By.linkText("Register")).click();
+		// define the chrome driver
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+
+		// define the drive instance
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		// nagivate the browser to this url
+		driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+		// Navigate to this button to create the booking
+		driver.findElement(By.linkText("Register")).click();
+
+		driver.findElement(By.name("yourName")).sendKeys("John1234");
+		driver.findElement(By.name("yourPassword")).sendKeys("123");
+		driver.findElement(By.name("yourEmail")).sendKeys("john123@gmail.com");
+
+		driver.findElement(By.id("submit_")).click();
+		
+		driver.quit();
+		
+	}
+	
+	@Test(priority=2)
+	public void edit() {
+		
+		// define the chrome driver
+				System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+
+				// define the drive instance
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+
+		driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+		// Edit the test auto booking
+		driver.findElement(By.xpath("//a[@href='UserServlet/edit?name=John1234']")).click();
+
+		driver.findElement(By.name("name")).clear();
+
+		driver.findElement(By.name("name")).sendKeys("John1234Edit");
+		
+//		driver.findElement(By.name("password")).sendKeys("123");
+
+		driver.findElement(By.name("email")).clear();
+
+		driver.findElement(By.name("email")).sendKeys("john123edit@gmail.com");
+
+		driver.findElement(By.id("edit")).click();
+
+		driver.quit();
+	}
+	
+	@Test(priority=3)
+	public void delete() {
+		// define the chrome driver
+				System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
+
+				// define the drive instance
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				
+		driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+
+		// Delete the test auto booking
+		driver.findElement(By.xpath("//a[@href='UserServlet/delete?name=John1234Edit']")).click();
+
+		driver.quit();
+	}
+
+//	@Test
+//	public void functinB() {
 //
-//      driver.findElement(By.name("yourName")).sendKeys("John1234");
-//      driver.findElement(By.name("yourPassword")).sendKeys("123");
-//      driver.findElement(By.name("yourEmail")).sendKeys("john123@gmail.com");
-//      
-//      driver.findElement(By.id("submit_")).click();
-//      
+//		// define the chrome driver
+//		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\chromedriver.exe");
 //
-//      driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
-//      //Edit the test auto booking 
-//      driver.findElement(By.xpath("//a[@href='UserServlet/edit?name=John1234']")).click();
-//      
-//      driver.findElement(By.name("name")).clear();
-//      
-//      driver.findElement(By.name("name")).sendKeys("John1234Edit");
-//      
-//      driver.findElement(By.name("email")).clear();
-//      
-//      driver.findElement(By.name("email")).sendKeys("john123edit@gmail.com");
-//      
-//      driver.findElement(By.id("edit")).click();
-//      
-//      driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
-//      
-//      //Delete the test auto booking
-//      driver.findElement(By.xpath("//a[@href='UserServlet/delete?name=John1234Edit']")).click();
-//      
+//		// define the drive instance
+//		WebDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		// nagivate the browser to this url
+//		driver.get("http://localhost:8090/DEVOPSRR/UserServlet");
+//		// Navigate to this button to create the booking
+//		driver.findElement(By.linkText("Register")).click();
+//
+//		driver.findElement(By.id("submit_")).click();
+//	}
 
-  
-  }
-  @BeforeTest
-  public void beforeTest() {
-  }
+	@BeforeTest
+	public void beforeTest() {
+	}
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@AfterTest
+	public void afterTest() {
+	}
 
 }
